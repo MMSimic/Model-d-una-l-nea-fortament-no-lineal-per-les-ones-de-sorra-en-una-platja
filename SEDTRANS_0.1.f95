4dc710f3b6fa8a9a2a591a8b2cdb1fbf0d94cdd4
Marcos <m.miros92@gmail.com>
2015-03-03 19:45:01 +0100

    3-3-15

--- SEDTRANS_0.1.f95
+++ SEDTRANS_0.1.f95
@@ -2,26 +2,34 @@ program sedtrans
 	winapp
     implicit none
     !character(len=10):: name
-    integer   ::  t,nx,ny,Lx,Ly,a,N,i
-	real::deltaT,deltaX,deltaY
-   ! real*8, allocatable,dimension(:):: x,y
-    real*8,dimension(12):: x,y
-    nx=12;ny=12
-!$$$$$$     allocate(x(nx))
-!$$$$$$     allocate(y(ny))
+    integer   ::  T,n,m,Lx,Ly,i
+	real :: deltaT,deltaX,deltaY
+    real,dimension(:,:),allocatable :: Q,beta,alpha
+    real*8,dimension(:),allocatable :: Cq,Hb,x,y
 
-    
-    do i=1,nx
+    print *, 'Number of ''x'' divisions?'
+    read *, n
+    allocate(x(n+1))
+    allocate(y(n+1))
+!$$$$$$     print *, 'Number of time divisions?'
+!$$$$$$     read *, T
+!$$$$$$     allocate()
+
+!$$$$$$     real*8,dimension(12):: x,y
+!$$$$$$     nx=12;m=12
+!$$$$$$     allocate(x(n))
+!$$$$$$     allocate(y(m))
+
+    !Set initial conditions
+    do i=1,n+1
       x(i)=i-1
-    enddo
-    do i=1,ny
-      y(i)=x(i)**2
+      y(i)=sin(x(i))/10+3
     enddo
 !$$$$$$     deallocate(x,y) 
 
-	i=winio@('%ww%ca[SIMPLEPLOT-Quadratic]%bg[grey]%pv&')
-	i=winio@('%pl[colour=black,x_array]&',400,250,nx,x,y)
-    i=winio@('%ff%nl%cn%tt[ok]')
+	i=winio@('%ww%ca[Sediment Transport Plot]%bg[grey]%pv&')
+	i=winio@('%pl[colour=red,y_min=0,x_array]&',400,250,n,x,y)
+    i=winio@('%ff%nl%cn%tt[Close]')
 !$$$$$$     !print *,'time?'
 !$$$$$$     !read*,t
 !$$$$$$     t=365
@@ -39,4 +47,15 @@ program sedtrans
 !$$$$$$    read *, pounds,pence
 !$$$$$$    total =100 * pounds + pence
 !$$$$$$    print *,'the total money in pence is ',total
+	CONTAINS
+    SUBROUTINE resize_array
+!$$$$$$         INTEGER,DIMENSION(:),ALLOCATABLE :: tmp_arr
+!$$$$$$         
+!$$$$$$         ALLOCATE(tmp_arr(2*SIZE(array)))
+!$$$$$$         tmp_arr(1:SIZE(array))=array
+!$$$$$$         DEALLOCATE(array)
+!$$$$$$         ALLOCATE(array(size(tmp_arr)))
+!$$$$$$         array=tmp_arr
+    
+    ENDSUBROUTINE resize_array
 end program sedtrans
